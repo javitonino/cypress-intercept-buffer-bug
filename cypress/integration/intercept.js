c80ea44016cf9af7764785302e080381cd48dba0
Javier Torres <javier@javiertorres.eu>
2021-01-13 17:15:49 +0100

    Dockerfiles with 6.2.0 and 6.2.1

--- cypress/integration/intercept.js
+++ cypress/integration/intercept.js
@@ -7,7 +7,7 @@ it('should intercept form data correctly without cy.intercept()', () => {
   });
 });
 
-it('should intercept form data correctly with cy.intercept()', () => {
+it('should intercept binary form data correctly with cy.intercept()', () => {
   cy.intercept('PUT', 'http://localhost:3000/upload', function (req) {
     req.reply((res) => {
       res.delay(1000).send();
@@ -22,3 +22,17 @@ it('should intercept form data correctly with cy.intercept()', () => {
   });
 });
 
+it('should intercept text form data correctly with cy.intercept()', () => {
+  cy.intercept('PUT', 'http://localhost:3000/upload', function (req) {
+    req.reply((res) => {
+      res.delay(1000).send();
+    });
+  });
+
+  cy.visit('http://localhost:3000/');
+  cy.get('input').attachFile('test.xml');
+  cy.get('button').click();
+  cy.get('span').should(function ($span) {
+    expect($span).to.have.text('SUCCESS')
+  });
+});
